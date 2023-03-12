73c482735b73b13ad36cc6129dc2979c6ff0e40a
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-03-12 22:44:31 +0200

    Add Material

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from PIL import Image
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
-from streamlit_option_menu import option_menu
 
 
 st.set_page_config(page_title= "My Webpage", page_icon=":tada:", layout= "wide")
