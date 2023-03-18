b219961e8b04aec96f68b968404d9c3b353f7a2d
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-03-18 23:28:49 +0200

    Initial commit

---
+++ learner_marks.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import streamlit as st
+
+# Load the Excel sheet
+df = pd.read_excel("student_marks.xlsx")
+
+# Convert columns to string type
+df["Name"] = df["Name"].astype(str)
+df["Password"] = df["Password"].astype(str)
+
+# Convert marks column to numeric type
+df["Marks"] = pd.to_numeric(df["Marks"], errors="coerce").astype(str)
+
+# Define the Streamlit app
+def app():
+    # Set the page title
+    st.set_page_config(page_title="Student Marks", page_icon=":mortar_board:")
+
+    # Set the app header
+    st.header("Technical Sciences Marks: March 2023")
+
+    # Define the input fields
+    name = st.text_input("Name")
+    password = st.text_input("Password", type="password")
+
+    # Filter the data based on the input fields
+    filtered_df = df[(df["Name"].str.strip() == name.strip()) & (df["Password"].str.strip() == password.strip())]
+
+    # Check if there is any data for the input fields
+    if filtered_df.empty:
+        st.warning("Incorrect name or password. Please try again.")
+    else:
+        # Get the student's marks
+        marks = filtered_df.iloc[0].get("Marks")
+        if marks == "nan":
+            st.warning("No marks found for this student.")
+        else:
+            st.success(f"Your marks out of 100 are: {marks}")
+
+# Run the app
+if __name__ == "__main__":
+    app()
