0d94a08804e5509507c921817e028c005cde39c5
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-03-19 16:27:08 +0200

    initial commit

--- learner_marks.py
+++ learner_marks.py
@@ -1,51 +1,25 @@
-import os
 import pandas as pd
 import streamlit as st
-from openpyxl import load_workbook
 
-# Set the path to the Excel sheet
-file_path = os.path.join(os.getcwd(), "data", "student_marks.xlsx")
+# Load the data
+data_folder = "data/"
+excel_file = "student_marks.xlsx"
+df = pd.read_excel(data_folder + excel_file)
 
-# Load the Excel sheet
-book = load_workbook(file_path, read_only=True)
-sheet = book.active
-data = sheet.values
-columns = next(data)
-df = pd.DataFrame(data, columns=columns)
+# Set up the Streamlit app
+st.title("Learner Marks Lookup")
 
-# Convert columns to string type
-df["Name"] = df["Name"].astype(str)
-df["Password"] = df["Password"].astype(str)
+# Add input fields for name and password
+name = st.text_input("Name:")
+password = st.text_input("Password:", type="password")
 
-# Convert marks column to numeric type
-df["Marks"] = pd.to_numeric(df["Marks"], errors="coerce").astype(str)
-
-# Define the Streamlit app
-def app():
-    # Set the page title
-    st.set_page_config(page_title="Student Marks", page_icon=":mortar_board:")
-
-    # Set the app header
-    st.header("Technical Sciences Marks: March 2023")
-
-    # Define the input fields
-    name = st.text_input("Name")
-    password = st.text_input("Password", type="password")
-
-    # Filter the data based on the input fields
-    filtered_df = df[(df["Name"].str.strip() == name.strip()) & (df["Password"].str.strip() == password.strip())]
-
-    # Check if there is any data for the input fields
-    if filtered_df.empty:
-        st.warning("Incorrect name or password. Please try again.")
+# Add a button to submit the inputs
+if st.button("Submit"):
+    # Check if the name and password match a row in the dataframe
+    result = df[(df["Name"]==name) & (df["Password"]==password)]
+    if len(result) > 0:
+        # Display the learner's mark
+        st.success(f"Your mark is {result.iloc[0]['Marks']}.")
     else:
-        # Get the student's marks
-        marks = filtered_df.iloc[0].get("Marks")
-        if marks == "nan":
-            st.warning("No marks found for this student.")
-        else:
-            st.success(f"Your marks out of 100 are: {marks}")
-
-# Run the app
-if __name__ == "__main__":
-    app()
+        # Display an error message if the name and password don't match
+        st.error("Invalid name or password.")
