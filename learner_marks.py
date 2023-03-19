f3f3dc738f88888e99162a52d9c304045459d58f
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-03-19 16:44:39 +0200

    Initial commit

--- learner_marks.py
+++ learner_marks.py
@@ -1,13 +1,14 @@
-import pandas as pd
+import openpyxl
 import streamlit as st
 
 # Load the data
 data_folder = "data/"
-excel_file = "student_marks.xlsx"
-df = pd.read_excel(data_folder + excel_file)
+excel_file = "learner_marks.xlsx"
+workbook = openpyxl.load_workbook(data_folder + excel_file)
+sheet = workbook.active
 
 # Set up the Streamlit app
-st.title("Technical Sciences March test Results: March 2023")
+st.title("Learner Marks Lookup")
 
 # Add input fields for name and password
 name = st.text_input("Name:")
@@ -15,11 +16,13 @@ password = st.text_input("Password:", type="password")
 
 # Add a button to submit the inputs
 if st.button("Submit"):
-    # Check if the name and password match a row in the dataframe
-    result = df[(df["Name"]==name) & (df["Password"]==password)]
-    if len(result) > 0:
-        # Display the learner's mark
-        st.success(f"Your mark is {result.iloc[0]['Marks']} out of 100.")
+    # Loop through each row in the sheet
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        # Check if the name and password match the current row
+        if name == row[0] and password == row[1]:
+            # Display the learner's mark
+            st.success(f"Your mark is {row[2]}.")
+            break
     else:
-        # Display an error message if the name and password don't match
+        # Display an error message if the name and password don't match any rows
         st.error("Invalid name or password.")
