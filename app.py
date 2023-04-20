dea177b1fcf9bbd9b39a54ab2093e7b73fde8ea9
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-20 07:56:31 +0200

    Google drive

--- app.py
+++ app.py
@@ -106,19 +106,19 @@ with st.container():
     st.write("---")
     st.title("GRADE 12 RESOURCES")
     st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
-    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1-OdRq6OwkDopuIZsSknbD9ipvRwFm1LJ?usp=share_link)")
+    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1-OdRq6OwkDopuIZsSknbD9ipvRwFm1LJ?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+12+Streamlit)")
 
 with st.container():
     st.write("---")
     st.title("GRADE 11 RESOURCES")
     st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
-    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1Bu5kOYvpsZ6JkSSgKcsVFtzAnpd6GcAs?usp=share_link)")
+    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1Bu5kOYvpsZ6JkSSgKcsVFtzAnpd6GcAs?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit)")
 
 with st.container():
     st.write("---")
     st.title("GRADE 10 RESOURCES")
     st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
-    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1U7EkFEwMrJPpAjN-4xX9VdoHWjy7340p?usp=share_link)")
+    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1U7EkFEwMrJPpAjN-4xX9VdoHWjy7340p?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+10+Streamlit)")
 
 with st.container():
     st.write("---")
