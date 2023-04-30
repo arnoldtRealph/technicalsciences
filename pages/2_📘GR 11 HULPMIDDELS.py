f843370c82b4e1a0876b2782a1a2de0eb3f7dcf6
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-04-30 16:18:18 +0200

    Pages

---
+++ pages/2_📘GR 11 HULPMIDDELS.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("GRADE 11 RESOURCES")
+st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
+st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1Bu5kOYvpsZ6JkSSgKcsVFtzAnpd6GcAs?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit&utm_id=G-7S7XQ3SXGP)")
---
+++ pages/3_📏GR 10 HULPMIDDELS.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("GRADE 10 RESOURCES")
+st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
+st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1U7EkFEwMrJPpAjN-4xX9VdoHWjy7340p?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+10+Streamlit&utm_id=G-55964DLV42)")
