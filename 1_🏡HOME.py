4e34d724a3aa25f66c4719f2937ca78f239e00a9
Arnoldt Visagie <visagie.arnoldt@gmail.com>
2023-05-13 13:12:13 +0200

    button

--- 1_🏡HOME.py
+++ 1_🏡HOME.py
@@ -6,6 +6,7 @@ from streamlit import components
 import os
 import pandas as pd
 from datetime import datetime
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title= "TECHSCI", layout= "wide")
 
@@ -103,25 +104,129 @@ with st.container():
     st.image(img_contact_form2)
     
 
-
 with st.container():
     st.write("---")
     st.title("GRADE 12 RESOURCES")
-    st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
-    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1-OdRq6OwkDopuIZsSknbD9ipvRwFm1LJ?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit&utm_id=G-QQREF8Y40R)")
+    st.subheader("Click on the following BUTTON to GET ACCESS to your STUDY MATERIAL.")
 
+#CODE FOR THE BUTTON
+button_html = '''
+<style>
+button {
+  border-radius: .25rem;
+  text-transform: uppercase;
+  font-style: normal;
+  font-weight: 400;
+  padding-left: 25px;
+  padding-right: 25px;
+  color: #fff;
+  -webkit-clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  height: 40px;
+  font-size: 0.7rem;
+  line-height: 14px;
+  letter-spacing: 1.2px;
+  transition: .2s .1s;
+  background-image: linear-gradient(90deg,#1c1c1c,#6220fb);
+  border: 0 solid;
+  overflow: hidden;
+}
+
+button:hover {
+  cursor: pointer;
+  transition: all .3s ease-in;
+  padding-right: 30px;
+  padding-left: 30px;
+}
+</style>
+<button onclick="window.open('https://drive.google.com/drive/folders/1-OdRq6OwkDopuIZsSknbD9ipvRwFm1LJ?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit&utm_id=G-QQREF8Y40R')" type="button">CLICK HERE</button>
+'''
+
+components.html(button_html)
+
+#NEW CONTAINER
 with st.container():
     st.write("---")
     st.title("GRADE 11 RESOURCES")
-    st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
-    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1Bu5kOYvpsZ6JkSSgKcsVFtzAnpd6GcAs?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit&utm_id=G-7S7XQ3SXGP)")
+    st.subheader("Click on the following BUTTON to GET ACCESS to your STUDY MATERIAL.")
 
+#CODE FOR THE BUTTON
+button_html = '''
+<style>
+button {
+  border-radius: .25rem;
+  text-transform: uppercase;
+  font-style: normal;
+  font-weight: 400;
+  padding-left: 25px;
+  padding-right: 25px;
+  color: #fff;
+  -webkit-clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  height: 40px;
+  font-size: 0.7rem;
+  line-height: 14px;
+  letter-spacing: 1.2px;
+  transition: .2s .1s;
+  background-image: linear-gradient(90deg,#1c1c1c,#6220fb);
+  border: 0 solid;
+  overflow: hidden;
+}
+
+button:hover {
+  cursor: pointer;
+  transition: all .3s ease-in;
+  padding-right: 30px;
+  padding-left: 30px;
+}
+</style>
+<button onclick="window.open('https://drive.google.com/drive/folders/1Bu5kOYvpsZ6JkSSgKcsVFtzAnpd6GcAs?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit&utm_id=G-7S7XQ3SXGP')" type="button">CLICK HERE</button>
+'''
+
+components.html(button_html)
+
+#NEW CONTAINER
 with st.container():
     st.write("---")
     st.title("GRADE 10 RESOURCES")
-    st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
-    st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1U7EkFEwMrJPpAjN-4xX9VdoHWjy7340p?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+10+Streamlit&utm_id=G-55964DLV42)")
+    st.subheader("Click on the following BUTTON to GET ACCESS to your STUDY MATERIAL.")
 
+# CODE FOR THE BUTTON
+button_html = '''
+<style>
+button {
+  border-radius: .25rem;
+  text-transform: uppercase;
+  font-style: normal;
+  font-weight: 400;
+  padding-left: 25px;
+  padding-right: 25px;
+  color: #fff;
+  -webkit-clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  height: 40px;
+  font-size: 0.7rem;
+  line-height: 14px;
+  letter-spacing: 1.2px;
+  transition: .2s .1s;
+  background-image: linear-gradient(90deg,#1c1c1c,#6220fb);
+  border: 0 solid;
+  overflow: hidden;
+}
+
+button:hover {
+  cursor: pointer;
+  transition: all .3s ease-in;
+  padding-right: 30px;
+  padding-left: 30px;
+}
+</style>
+<button onclick="window.open('https://drive.google.com/drive/folders/1U7EkFEwMrJPpAjN-4xX9VdoHWjy7340p?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+10+Streamlit&utm_id=G-55964DLV42')" type="button">CLICK HERE</button>
+'''
+
+components.html(button_html)
+
+#NEW CONTAINER
 with st.container():
     st.write("---")
 st.header(" MOTIVATIONAL QUOTES :thought_balloon:")
--- pages/4_📘GR 11 HULPMIDDELS.py
+++ pages/4_📘GR 11 HULPMIDDELS.py
@@ -1,6 +1,40 @@
 import streamlit as st
-
+import streamlit.components.v1 as components
 
 st.title("GRADE 11 RESOURCES")
-st.subheader("Click on the following link to GET ACCESS to your STUDY MATERIAL.")
-st.write("[CLICK HERE >](https://drive.google.com/drive/folders/1Bu5kOYvpsZ6JkSSgKcsVFtzAnpd6GcAs?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit&utm_id=G-7S7XQ3SXGP)")
+st.subheader("Click on the following BUTTON to GET ACCESS to your STUDY MATERIAL.")
+
+#CODE FOR THE BUTTON
+button_html = '''
+<style>
+button {
+  border-radius: .25rem;
+  text-transform: uppercase;
+  font-style: normal;
+  font-weight: 400;
+  padding-left: 25px;
+  padding-right: 25px;
+  color: #fff;
+  -webkit-clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  clip-path: polygon(0 0,0 0,100% 0,100% 0,100% calc(100% - 15px),calc(100% - 15px) 100%,15px 100%,0 100%);
+  height: 40px;
+  font-size: 0.7rem;
+  line-height: 14px;
+  letter-spacing: 1.2px;
+  transition: .2s .1s;
+  background-image: linear-gradient(90deg,#1c1c1c,#6220fb);
+  border: 0 solid;
+  overflow: hidden;
+}
+
+button:hover {
+  cursor: pointer;
+  transition: all .3s ease-in;
+  padding-right: 30px;
+  padding-left: 30px;
+}
+</style>
+<button onclick="window.open('https://drive.google.com/drive/folders/1Bu5kOYvpsZ6JkSSgKcsVFtzAnpd6GcAs?usp=share_link&utm_source=Google+Drive&utm_medium=Shared+link&utm_campaign=Grade+11+Streamlit&utm_id=G-7S7XQ3SXGP')" type="button">CLICK HERE</button>
+'''
+
+components.html(button_html)
